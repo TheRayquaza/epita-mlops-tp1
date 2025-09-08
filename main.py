78519d9665f289c45b1f4aad8f2139116cace94a
Matéo Lelong <mateo@forge.epita.fr>
2025-09-08 15:15:19 +0200

    ci: add ci to build docker image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import joblib
 import train_model
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 import numpy as np
 import uvicorn
@@ -19,6 +20,10 @@ async def predict(data: PredictData):
     input_array = np.array(list(data.dict().values())).reshape(1, -1)
     pred = model.predict(input_array)
     return {"prediction": float(pred[0])}
+
+@app.get("/redirect")
+async def redirect_to_youtube():
+    return RedirectResponse("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=int(os.getenv('PORT')), host=str(os.getenv('HOST')), log_level="info")
