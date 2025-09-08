9e8786d4d4d4d81f0af605f33a3a221527a6f253
Matéo Lelong <mateo@forge.epita.fr>
2025-09-08 15:53:07 +0200

    feat: add frontend

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ ENV PATH=/home/appuser/.local/bin:$PATH
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY regression.joblib main.py /app/
+COPY regression.joblib main.py index.html /app/
 
 RUN useradd -m appuser && chown -R appuser:appuser /app
 USER appuser
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import joblib
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from pydantic import BaseModel
 import numpy as np
 import uvicorn
@@ -20,9 +20,16 @@ async def predict(data: PredictData):
     pred = model.predict(input_array)
     return {"prediction": float(pred[0])}
 
-@app.get("/")
+@app.get("/yt")
 async def redirect_to_youtube():
     return RedirectResponse("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+@app.get("/")
+async def root():
+    content = ""
+    with open("index.html", "r") as file:
+        content = file.read()
+    return HTMLResponse(content=content)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=int(os.getenv('PORT')), host=str(os.getenv('HOST')), log_level="info")
